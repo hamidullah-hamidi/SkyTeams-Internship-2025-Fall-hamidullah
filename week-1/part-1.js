4e178e59b70ad894417737e5417e0137ebfc6db6
Hamidullah hamidi <hamidullahh754@gmail.com>
2025-11-19 12:54:29 +0430

    solve comments after code review

--- week-1/part-1.js
+++ week-1/part-1.js
@@ -9,15 +9,18 @@ const swapValues = () => {
 
   a = b;
   b = temp;
-  return { a: a, b: b };
+  return {a, b };
 };
 // console.log(swapValues()); // a = 20, b = 10
 
 // Check if a number is even, odd, or prime
 const checkNumber = (num) => {
+  if(num === 2) return 'even and prime'
+  if(num < 2 ) return n % 2 === 0 ? 'even' : 'false'
+  
   if (num % 2 === 0) return 'even';
 
-  for (let i = 2; i <= Math.sqrt(num); i++) {
+  for (let i = 3; i <= Math.sqrt(num); i += 2) {
     if (num % i === 0) return 'odd';
   }
 
