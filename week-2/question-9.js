c497d21d2db9d5d63da643fa5d02126453670c1a
Hamidullah hamidi <hamidullahh754@gmail.com>
2025-11-20 16:24:09 +0430

    comments resolved

--- week-2/question-9.js
+++ week-2/question-9.js
@@ -7,13 +7,14 @@
 // B
 
 function gradeCalculator(grade) {
-  if (grade > 85 && grade <= 100) return 'A';
-  if (grade > 70 && grade <= 85) return 'B';
-  if (grade > 55 && grade <= 70) return 'C';
-  if (grade > 40 && grade <= 55) return 'D';
-  if (grade > 25 && grade <= 40) return 'E';
-  if (grade >= 0 && grade <= 25) return 'F';
   if (grade > 100 || grade < 0) return 'Invalid';
+
+  if (grade > 85) return 'A';
+  if (grade > 70) return 'B';
+  if (grade > 55) return 'C';
+  if (grade > 40) return 'D';
+  if (grade > 25) return 'E';
+  if (grade >= 0) return 'F';
 }
 
 console.log(gradeCalculator(85));
