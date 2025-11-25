368f1efe7efd84482024d5421d9b2c643a002fa0
Hamidullah hamidi <hamidullahh754@gmail.com>
2025-11-26 00:01:58 +0430

    use divide by 10 instead of change to string

--- week-3/question-2.js
+++ week-3/question-2.js
@@ -12,9 +12,22 @@
 
 export function reverseNumber(num) {
   // way -1
-  const reversed = Math.abs(num).toString().split('').reverse().join('');
+  let originalNum = num;
+  let reversed = 0;
+
+  while (num !== 0) {
+    const digit = num % 10;
+    reversed = reversed * 10 + digit;
+
+    num = Math.floor(num / 10);
+  }
+
+  return originalNum < 0 ? originalNum === -reversed : originalNum === reversed;
 
   // way -2
+  // const reversed = Math.abs(num).toString().split('').reverse().join('');
+
+  // way -3
   // let reversed = '';
   // let numString = Math.abs(num).toString();
 
@@ -22,7 +35,7 @@ export function reverseNumber(num) {
   //   reversed += numString[i];
   // }
 
-  return num < 0 ? -+reversed : +reversed;
+  // return num < 0 ? -+reversed : +reversed;
 }
 
 console.log(reverseNumber(-1234));
