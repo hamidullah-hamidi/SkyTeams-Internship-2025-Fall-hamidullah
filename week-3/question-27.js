cbbfb1ee77f1330faafcc7256a88c0ff8b8b5b0f
Hamidullah hamidi <hamidullahh754@gmail.com>
2025-12-01 00:04:20 +0430

    resolve all comments

--- week-3/question-27.js
+++ week-3/question-27.js
@@ -8,27 +8,45 @@
 // Input:
 //
 // Output:
-// [1, 2, 3, 4, 5]
+// [1, 3, 5]
 
-//* way -1
-export const findUniqueElements = (arr) => [...new Set(arr)];
+//* way 1
+
+export const findUniqueElements = (arr) => {
+  const obj = {};
+  const uniqueElements = [];
+
+  for (let num of arr) {
+    if (obj[num]) obj[num] = obj[num] + 1;
+    else obj[num] = 1;
+  }
+
+  for (let [key, _] in obj) {
+    if (obj[key] === 1) uniqueElements.push(key);
+  }
+
+  return uniqueElements;
+};
+
+console.log(findUniqueElements([1, 2, 2, 3, 4, 4, 5]));
 
 //* way -2
 /*
 export const findUniqueElements = (arr) => {
   let uniqueArray = new Set();
   let uniqueArray1 = [];
-
+  
   for (let el of arr) {
     uniqueArray.add(el);
-  }
+    }
+    
+    for (let el of uniqueArray.values()) {
+      uniqueArray1.push(el);
+      }
+      
+      return uniqueArray1;
+      };
+      */
 
-  for (let el of uniqueArray.values()) {
-    uniqueArray1.push(el);
-  }
-
-  return uniqueArray1;
-};
-*/
-
-console.log(findUniqueElements([1, 2, 2, 3, 4, 4, 5]));
+//* way -3
+// export const findUniqueElements = (arr) => [...new Set(arr)];
--- week-3/question-28.js
+++ week-3/question-28.js
@@ -10,6 +10,25 @@
 // Output:
 // [3, 1, 5]
 
-export const removeDuplicatesFromArray = (arr) => [...new Set(arr)];
+//* way 1
+export const removeDuplicatesFromArray = (arr) => {
+  let obj = {};
+  let result = [];
+
+  for (let num of arr) {
+    if (obj[num]) obj[num] = obj[num] + 1;
+    else obj[num] = 1;
+  }
+  console.log(obj);
+
+  for (let [key, _] in obj) {
+    result.push(key);
+  }
+
+  return result;
+};
 
 console.log(removeDuplicatesFromArray([3, 1, 3, 5, 1]));
+
+//* way 2
+// export const removeDuplicatesFromArray = (arr) => [...new Set(arr)];
