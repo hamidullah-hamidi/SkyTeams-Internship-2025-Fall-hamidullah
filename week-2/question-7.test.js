70f3182e5ac096b2b05eb25462086b79aaffd95b
Hamidullah hamidi <hamidullahh754@gmail.com>
2025-12-01 12:04:13 +0430

    create branch week-4

--- week-2/question-7.test.js
+++
@@ -1,6 +0,0 @@
-const checkOddOrEven = require('./question-7');
-
-test('if number is divisible by 2 return even else return odd', () => {
-  expect(checkOddOrEven(7)).toBe('odd');
-  expect(checkOddOrEven(8)).toBe('even');
-});
