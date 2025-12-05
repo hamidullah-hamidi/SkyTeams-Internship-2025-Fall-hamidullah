7d40b65c6e9a74427b2e8388a7e1ebf04c931abf
Hamidullah hamidi <hamidullahh754@gmail.com>
2025-12-05 20:40:56 +0430

    comment on question-2 resolved

--- week-4/question-2.js
+++ week-4/question-2.js
@@ -9,19 +9,30 @@
 // Output: [1, 2, 3, 4, 5, 6]
 
 export const mergeTwoSortedArrays = (arr1, arr2) => {
-  let result = [...arr1, ...arr2];
+  let result = [];
 
-  for (let i = 0; i < result.length - 1; i++) {
-    let temp = 0;
-    for (let j = 0; j < result.length; j++) {
-      if (result[j] > result[j + 1]) {
-        temp = result[j + 1];
-        result[j + 1] = result[j];
-        result[j] = temp;
-      }
+  let i = 0,
+    j = 0;
+
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] <= arr2[j]) {
+      result.push(arr1[i]);
+      i++;
+    } else {
+      result.push(arr2[j]);
+      j++;
     }
+  }
+
+  while (i < arr1.length) {
+    result.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    result.push(arr2[j]);
+    j++;
   }
 
   return result;
 };
-console.log(mergeTwoSortedArrays([1, 3, 5], [2, 4, 6]));
+console.log(mergeTwoSortedArrays([1, 3], [2, 4, 6]));
