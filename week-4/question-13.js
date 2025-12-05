5e30fbf8320972350f018c3d64036f28e9d4449f
Hamidullah hamidi <hamidullahh754@gmail.com>
2025-12-05 10:54:07 +0430

    work questions 13 with obj instead of Map

--- week-4/question-13.js
+++ week-4/question-13.js
@@ -9,6 +9,7 @@
 export const stringPattern = (pattern, sentence) => {
   const map1 = new Map();
   const map2 = new Map();
+
   const arr = sentence.split(' ');
 
   if (pattern.length !== arr.length) return false;
@@ -17,7 +18,6 @@ export const stringPattern = (pattern, sentence) => {
     if (map1.has(pattern[i])) {
       if (map1.get(pattern[i]) !== arr[i]) return false;
     } else map1.set(pattern[i], arr[i]);
-
     if (map2.has(arr[i])) {
       if (map2.get(arr[i]) !== pattern[i]) return false;
     } else map2.set(arr[i], pattern[i]);
@@ -27,3 +27,18 @@ export const stringPattern = (pattern, sentence) => {
 };
 
 console.log(stringPattern('abba', 'dog cat cat dog'));
+
+/* with obj:
+
+const obj1 = {};
+const obj2 = {};
+
+if (obj1[pattern[i]]) {
+  if (obj1[pattern[i]] !== arr[i]) return false;
+} else obj1[pattern[i]] = arr[i];
+
+if (obj2[arr[i]]) {
+  if (obj2[arr[i]] !== pattern[i]) return false;
+} else obj2[arr[i]] = pattern[i];
+
+*/
