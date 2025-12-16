9673a2ebd80704188be211dbd194f0be01fcba97
Hamidullah hamidi <hamidullahh754@gmail.com>
2025-12-16 15:26:23 +0430

    work question-25.js week-4

--- week-4/question-25.js
+++ week-4/question-25.js
@@ -6,7 +6,33 @@
 // If an element is zero, set its entire row and column to zero.
 // Follow-up: solve in constant extra space using the matrix itself as markers.
 
-export const setMatrixZeros = (matrix) => {};
+export const setMatrixZeros = (matrix) => {
+  if (!matrix || matrix.length === 0 || matrix[0].length === 0) return matrix;
+
+  const rowsLength = matrix.length;
+  const colsLength = matrix[0].length;
+  const rows = new Array(rowsLength).fill(false);
+  const cols = new Array(colsLength).fill(false);
+
+  for (let i = 0; i < rowsLength; i++) {
+    for (let j = 0; j < colsLength; j++) {
+      if (matrix[i][j] === 0) {
+        rows[i] = true;
+        cols[j] = true;
+      }
+    }
+  }
+
+  for (let i = 0; i < rowsLength; i++) {
+    for (let j = 0; j < colsLength; j++) {
+      if (rows[i] || cols[j]) {
+        matrix[i][j] = 0;
+      }
+    }
+  }
+
+  return matrix;
+};
 
 console.table(
   setMatrixZeros([
@@ -15,7 +41,3 @@ console.table(
     [4, 3, 2],
   ])
 );
-
-// 1 2 3      1 0 3
-// 2 0 5  =>  0 0 0
-// 4 3 2      4 0 2
