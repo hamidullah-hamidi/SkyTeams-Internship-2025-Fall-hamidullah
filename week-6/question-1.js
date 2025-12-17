d06e7778f46d85736de5c2511c0afff814b82b30
Hamidullah hamidi <hamidullahh754@gmail.com>
2025-12-17 10:34:01 +0430

    implement treeHight without recursion

--- week-6/question-1.js
+++ week-6/question-1.js
@@ -1,8 +1,7 @@
 // Author: hamidullah hamidi
 // Date: 2025/16/11
 
-// Question:
-// Height of Binary Tree
+// Question:  Height of Binary Tree
 // Write a function that returns the number of levels in a binary tree.
 // An empty tree has height 0, and a tree with only one node has height 1.
 
@@ -19,17 +18,34 @@ root.left = new TreeNode(2);
 root.right = new TreeNode(3);
 root.left.left = new TreeNode(4);
 root.left.right = new TreeNode(5);
-root.right.left = new TreeNode(4);
-root.right.right = new TreeNode(5);
 
 export const treeHeight = (root) => {
   if (!root) return 0;
-  return 1 + Math.max(treeHeight(root.left), treeHeight(root.right));
+
+  let hight = 0;
+  const list = [root];
+
+  // [1]
+  // [2,3]
+  // [4,5]
+
+  while (list.length !== 0) {
+    let levelSize = list.length;
+
+    for (let i = 0; i < levelSize; i++) {
+      let current = list.shift();
+
+      if (current.left) list.push(current.left);
+      if (current.right) list.push(current.right);
+    }
+    hight++;
+  }
+
+  return hight;
+
+  //  with recursion (simple & short):
+  // return 1 + Math.max(treeHeight(root.left), treeHeight(root.right));
 };
 
 // console.log(treeHeight());
-// console.log(treeHeight(root));
-
-//     1      => 1
-//  2    3    => 1 + 1
-// 4 5  4 5   => 1 + 1 + 1  => 3
+console.log(treeHeight(root));
